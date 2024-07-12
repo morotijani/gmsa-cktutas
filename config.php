46830e4b561952d58d4683a7327a744c82faf098
morotijani <tjhackx111@gmail.com>
2024-07-12 17:07:24 +0000

    initial commit

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+
+	define('BASEURL', $_SERVER['DOCUMENT_ROOT'].' /gmsa-cktutas/');
+	
+	define('PROOT', '/gmsa-cktutas/');
+
+	define('USER_CART_COOKIE', 'GTPez3FE45ghAoPj76');
+	define('CART_COOKIE', 'TZz3FE45ghAoPj76');
+
+	// exit for 30days
+	define('CART_COOKIE_EXPIRE', time() + (86400 * 30));
+
+	define('TAXRATE', '2');
+	define('SHIPPING', '0.99');
+
+	define('CURRENCY', 'USD');	
+
+	define('RECAPTCHA_SITE_KEY_PUBLIC', '6LejwV0jAAAAAAqPOSLq6mRMRZNnK5di3PQ3iRRj');
+	define('RECAPTCHA_SITE_KEY_SECRETE', '6LejwV0jAAAAAMpUdo_o-AcE_-5ciCZXJrBg3ATk');
+
+	define('RECAPTCHA_KEY', '6Lcx1lMjAAAAAPzgKTIkoH30gNPQ9-xVe49ssSLC');
+
+	define('MAIL_MAIL', 'info@garypie.com');
+
+	define('MAIL_KEY', 'Hn4f44da2');
+
+	define('IPINFO_KEY', 'c5c08603163207');
+
