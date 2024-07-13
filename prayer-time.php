e97aad59310f1e4c77ca7180b01b07652c1f6945
morotijani <tjhackx111@gmail.com>
2024-07-13 12:47:10 +0000

    initial commit

--- prayer-time.php
+++ prayer-time.php
@@ -17,7 +17,70 @@
    // include ("inc/nav.inc.php");
 
 
-?>
+?><header class="header-sticky header-absolute" data-bs-theme="<?= $navTheme; ?>">
+        <nav class="navbar navbar-expand-xl px-lg-5">
+            <div class="container-fluid">
+                <a class="navbar-brand me-5" href="index.html">
+                    <img class="light-mode-item navbar-brand-item" src="<?= PROOT; ?>assets/media/logo/logo.png" alt="logo">
+                    <img class="dark-mode-item navbar-brand-item" src="<?= PROOT; ?>assets/media/logo/logo-1.jpeg" alt="logo">
+                </a>
+
+                <ul class="nav align-items-center dropdown-hover ms-sm-2">
+                    <li class="nav-item dropdown dropdown-animation">
+                        <button class="btn btn-link mb-0 px-2 lh-1" id="bd-theme"
+                        type="button"
+                        aria-expanded="false"
+                        data-bs-toggle="dropdown"
+                        data-bs-display="static">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"  class="bi bi-circle-half theme-icon-active fill-mode fa-fw" viewBox="0 0 16 16">
+                            <path d="M8 15A7 7 0 1 0 8 1v14zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16z"/>
+                            <use href="#"></use>
+                        </svg>
+                        </button>
+
+                        <ul class="dropdown-menu min-w-auto dropdown-menu-end" aria-labelledby="bd-theme">
+                            <li class="mb-1">
+                                <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="light">
+                                    <svg width="16" height="16" fill="currentColor" class="bi bi-brightness-high-fill fa-fw mode-switch me-1" viewBox="0 0 16 16">
+                                        <path d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
+                                        <use href="#"></use>
+                                    </svg>Light
+                                </button>
+                            </li>
+                            <li class="mb-1">
+                                <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="dark">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-moon-stars-fill fa-fw mode-switch me-1" viewBox="0 0 16 16">
+                                        <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z"/>
+                                        <path d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.734 1.734 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.734 1.734 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.734 1.734 0 0 0 1.097-1.097l.387-1.162zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L13.863.1z"/>
+                                        <use href="#"></use>
+                                    </svg>Dark
+                                </button>
+                            </li>
+                            <li>
+                                <button type="button" class="dropdown-item d-flex align-items-center active" data-bs-theme-value="auto">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-circle-half fa-fw mode-switch me-1" viewBox="0 0 16 16">
+                                        <path d="M8 15A7 7 0 1 0 8 1v14zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16z"/>
+                                        <use href="#"></use>
+                                    </svg>Auto
+                                </button>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item">
+                        <button class="navbar-toggler ms-3 p-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-animation">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </span>
+                        </button>
+                    </li>   
+                </ul>
+
+            </div>
+        </nav>
+    </header>
     <main>
 
         // get with time zone
@@ -37,17 +100,78 @@
                     <div class="p-2" id="today-date" style="font-size: 48px; font-weight: 400;"></div>
                 </div>
 
-                <div class="d-flex flex-row-reverse mt-3">
-                    <div class="card text-end mr-2" style="">
+                <div class="d-flex flex-row-reverse gap-3 mt-3">
+                    <div class="card text-end" style="">
                         <div class="card-body">
                             <h5 class="card-title" style="font-size: xx-large; font-weight: 900">Fajr</h5>
-                            <p class="card-text lead">12:22 AM.</p>
+                            <p class="card-text fw-light lh-base lead">12:22 AM.</p>
                         </div>
                     </div>
-                    <div class="card text-end ml-2" style="">
+                    <div class="card text-end" style="">
                         <div class="card-body">
                             <h5 class="card-title" style="font-size: xx-large; font-weight: 900">Zuhr</h5>
-                            <p class="card-text lead">43:43 PM.</p>
+                            <p class="card-text fw-light lh-base lead">43:43 PM.</p>
+                        </div>
+                    </div>
+                    <div class="card text-end" style="">
+                        <div class="card-body">
+                            <h5 class="card-title" style="font-size: xx-large; font-weight: 900">Magrib</h5>
+                            <p class="card-text fw-light lh-base lead">43:43 PM.</p>
+                        </div>
+                    </div>
+                    <div class="card text-end" style="">
+                        <div class="card-body">
+                            <h5 class="card-title" style="font-size: xx-large; font-weight: 900">Isha</h5>
+                            <p class="card-text fw-light lh-base lead">43:43 PM.</p>
+                        </div>
+                    </div>
+                    <div class="card text-end" style="">
+                        <div class="card-body">
+                            <h5 class="card-title" style="font-size: xx-large; font-weight: 900">Jumm'ah</h5>
+                            <p class="card-text fw-light lh-base lead">43:43 PM.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="pt-0">
+            <div class="container">
+                <!-- Title -->
+                <div class="inner-container-small text-center mb-4 mb-sm-6">
+                    <h2 class="mb-3">General Time</h2>
+                    <p class="mb-0">Our friendly team members are always willing to help you understand your present technology requirements and provide suggestions on your future needs.</p>
+                </div>
+    
+                <div class="row g-4 g-lg-6">
+                    <div class="col-lg-9 mx-auto">
+                        <div class="tab-content" id="pills-tabContent">
+                            <div class="card card-body bg-light d-md-flex justify-content-md-between align-items-md-center flex-md-row p-4 mb-3">
+                                <div class="mb-4 mb-md-0">
+                                    <div class="badge text-bg-dark mb-3">Information Technology</div>
+                                    <h5 class="mb-0"><a href="career-single.html" class="stretched-link">UI/UX and Product Designer</a></h5>
+                                    <div class="hstack gap-3 gap-sm-4 flex-wrap mt-3">
+                                        <span><i class="bi bi-geo-alt me-2"></i>USA, ontario</span>
+                                        <span><i class="bi bi-clock me-2"></i>San Francisco</span>
+                                        <span><i class="bi bi-briefcase me-2"></i>$32,000</span>
+                                    </div>
+                                </div>
+                                <div><a href="#" class="btn btn-primary mb-0">Apply now<i class="bi bi-arrow-right ms-2"></i></a></div>
+                            </div>
+
+                            <div class="card card-body bg-light d-md-flex justify-content-md-between align-items-md-center flex-md-row p-4 mb-3">
+                                <div class="mb-4 mb-md-0">
+                                    <div class="badge text-bg-dark mb-3">Business</div>
+                                    <h5 class="mb-0"><a href="career-single.html" class="stretched-link">General Office Manager</a></h5>
+        
+                                    <div class="hstack gap-3 gap-sm-4 flex-wrap mt-3">
+                                        <span><i class="bi bi-geo-alt me-2"></i>USA, New York</span>
+                                        <span><i class="bi bi-clock me-2"></i>Full time</span>
+                                        <span><i class="bi bi-briefcase me-2"></i>$41,000</span>
+                                    </div>
+                                </div>
+                                <div><a href="#" class="btn btn-primary mb-0">Apply now<i class="bi bi-arrow-right ms-2"></i></a></div>
+                            </div>
                         </div>
                     </div>
                 </div>
