f3ad33c6e8a955606dbeab508254b567c962bb2e
morotijani <tjhackx111@gmail.com>
2024-07-13 09:26:53 +0000

    initial commit

--- index.php
+++ index.php
@@ -1,121 +1,13 @@
 <?php
 
+    // Index page
+
     require_once ("db_connection/conn.php");
+    $navTheme = "dark";
+    include ("inc/header.inc.php");
 
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Welcome - GMSA CKTUTAS</title>
-
-    <!-- Meta Tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="author" content="gmsacktutas.com">
-    <meta name="description" content="">
-
-    <!-- Dark mode -->
-    <script src="<?= PROOT; ?>dist/js/dark-mode.js"></script>
-
-    <link rel="shortcut icon" href="<?= PROOT; ?>assets/media/logo/logo-1.jpeg">
-    
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-
-    <link rel="stylesheet" type="text/css" href="<?= PROOT; ?>dist/css/all.min.css">
-    <link rel="stylesheet" type="text/css" href="<?= PROOT; ?>dist/css/bootstrap-icons.min.css">
-    <link rel="stylesheet" type="text/css" href="<?= PROOT; ?>dist/css/swiper-bundle.min.css">
-    
-    <!-- Theme CSS -->
-    <link rel="stylesheet" type="text/css" href="<?= PROOT; ?>dist/css/gmsa.css">
-    <link rel="stylesheet" type="text/css" href="<?= PROOT; ?>dist/css/main.css">
-
-</head>
-<body>
-
-    <!-- Header START -->
-    <header class="header-sticky header-absolute" data-bs-theme="dark">
-        <nav class="navbar navbar-expand-xl px-lg-5">
-            <div class="container-fluid">
-                <a class="navbar-brand me-5" href="index.html">
-                    <img class="light-mode-item navbar-brand-item" src="<?= PROOT; ?>assets/media/logo/logo.png" alt="logo">
-                    <img class="dark-mode-item navbar-brand-item" src="<?= PROOT; ?>assets/media/logo/logo-1.jpeg" alt="logo">
-                </a>
-                <div class="navbar-collapse collapse" id="navbarCollapse">
-                    <ul class="navbar-nav navbar-nav-scroll dropdown-hover mx-auto">
-                        <li class="nav-item dropdown"> <a class="nav-link active" href="<?= PROOT; ?>">Home</a></li>
-                        <li class="nav-item dropdown"> <a class="nav-link" href="<?= PROOT; ?>executives">Executives</a></li>
-                        <li class="nav-item dropdown"> <a class="nav-link" href="<?= PROOT; ?>calendar">Calendar</a></li>
-                        <li class="nav-item dropdown"> <a class="nav-link" href="<?= PROOT; ?>prayer-time">Prayer Time</a></li>
-                        <li class="nav-item dropdown"> <a class="nav-link" href="<?= PROOT; ?>news">News</a></li>
-                        <li class="nav-item"> <a class="nav-link" href="<?= PROOT; ?>contact-us">Contact us</a> </li>
-                    </ul>
-                </div>
-
-                <ul class="nav align-items-center dropdown-hover ms-sm-2">
-                    <li class="nav-item dropdown dropdown-animation">
-                        <button class="btn btn-link mb-0 px-2 lh-1" id="bd-theme"
-                        type="button"
-                        aria-expanded="false"
-                        data-bs-toggle="dropdown"
-                        data-bs-display="static">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"  class="bi bi-circle-half theme-icon-active fill-mode fa-fw" viewBox="0 0 16 16">
-                            <path d="M8 15A7 7 0 1 0 8 1v14zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16z"/>
-                            <use href="#"></use>
-                        </svg>
-                        </button>
-
-                        <ul class="dropdown-menu min-w-auto dropdown-menu-end" aria-labelledby="bd-theme">
-                            <li class="mb-1">
-                                <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="light">
-                                    <svg width="16" height="16" fill="currentColor" class="bi bi-brightness-high-fill fa-fw mode-switch me-1" viewBox="0 0 16 16">
-                                        <path d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
-                                        <use href="#"></use>
-                                    </svg>Light
-                                </button>
-                            </li>
-                            <li class="mb-1">
-                                <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="dark">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-moon-stars-fill fa-fw mode-switch me-1" viewBox="0 0 16 16">
-                                        <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z"/>
-                                        <path d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.734 1.734 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.734 1.734 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.734 1.734 0 0 0 1.097-1.097l.387-1.162zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L13.863.1z"/>
-                                        <use href="#"></use>
-                                    </svg>Dark
-                                </button>
-                            </li>
-                            <li>
-                                <button type="button" class="dropdown-item d-flex align-items-center active" data-bs-theme-value="auto">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-circle-half fa-fw mode-switch me-1" viewBox="0 0 16 16">
-                                        <path d="M8 15A7 7 0 1 0 8 1v14zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16z"/>
-                                        <use href="#"></use>
-                                    </svg>Auto
-                                </button>
-                            </li>
-                        </ul>
-                    </li>
-
-                    <li class="nav-item position-relative ms-2 ms-sm-3"><a class="btn btn-sm btn-light mb-0">Register</a></li>
-                    <li class="nav-item position-relative ms-2 ms-sm-3"><a class="btn btn-sm btn-success mb-0">Pay dues</a></li>
-
-                    <li class="nav-item">
-                        <button class="navbar-toggler ms-3 p-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-animation">
-                                <span></span>
-                                <span></span>
-                                <span></span>
-                            </span>
-                        </button>
-                    </li>   
-                </ul>
-
-            </div>
-        </nav>
-    </header>
-
-    <main>
 
         <section class="pt-0">
             <div class="swiper overflow-hidden" data-swiper-options='{
