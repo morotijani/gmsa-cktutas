23f36226526b8b696881c467d46cd3d1a35b3ddb
morotijani <tjhackx111@gmail.com>
2024-07-14 08:36:35 +0000

    initial commit

--- news.php
+++ news.php
@@ -21,16 +21,91 @@
     
     <main>
 
-        <section class="">
-            <div class="container-fluid">
-                <h2 class="text-decorated text-decorated-padding text-uppercase font-weight-bold">Full <br>calendar</h2>
-                
-                <div id='calendar' class="mb-5"></div>
+        <section class="pt-8">
+            <div class="container">
+                <div class="inner-container text-center mb-6">
+                    <h1 class="mb-0 lh-base position-relative">
+                        <span class="position-absolute top-0 start-0 mt-n5 ms-n5 d-none d-sm-block">
+                            <svg class="fill-primary" width="63.6px" height="93.3px" viewBox="0 0 63.6 93.3" style="enable-background:new 0 0 63.6 93.3;" xml:space="preserve">
+                                <path d="M58.5,1.9c0.5,0,1.6,5.3,2.4,11.8c0.8,6.5,1.4,14,1.6,18.5c0.3,8.8-0.5,15.9-1.6,16c-1.1,0-2.1-7.1-2.4-15.8 c-0.2-4.4-0.3-12-0.4-18.4C57.9,7.3,57.9,1.9,58.5,1.9z"/>
+                                <path d="M47.7,44.4c-0.5,0.1-1.5-2.1-2.8-4.7c-1.3-2.6-2.8-5.5-3.7-7.2c-1.7-3.4-2.9-6.4-2.1-7c0.8-0.6,3.4,1.5,5.3,5.1 c1,1.8,2.2,5.1,2.9,8.1C48.1,41.8,48.2,44.3,47.7,44.4z"/>
+                                <path d="M36.2,53.4c-0.2,0.5-4.1-1.2-8.5-3.5c-4.4-2.3-9.5-5.4-12.3-7.3c-5.6-3.9-9.6-7.9-9-8.8c0.6-0.9,5.4,1.7,11,5.5 c2.8,1.9,7.5,5.3,11.6,8.2C33.1,50.5,36.4,53,36.2,53.4z"/>
+                                <path d="M27.2,68.3c-0.1,0.5-2.1,0.7-4.4,0.6c-2.4,0-5.1-0.3-6.7-0.7c-3.1-0.6-5.4-2-5.2-3c0.2-1,2.9-1.2,5.9-0.5 c1.5,0.3,4.1,1,6.3,1.7C25.4,67.1,27.2,67.8,27.2,68.3z"/>
+                                <path d="M30.8,83.2c0.1,0.5-3.5,1.7-7.7,3.1c-4.3,1.4-9.2,3.1-12.1,4.1c-5.7,1.9-10.6,3.1-11,2.1 c-0.4-0.9,3.9-3.6,9.8-5.6c2.9-1,8.1-2.4,12.6-3.2C26.9,83,30.7,82.7,30.8,83.2z"/>
+                            </svg>
+                        </span>
+                        Thoughts, stories and ideas from GMSA - CKTUTAS
+                    </h1>
 
-                <div class="row" data-aos="fade-up">
-                    <div class="col text-center">
-                        <p><span class="badge bg-success text-blue mr-1">New</span> GMSA gives you all prayer time and details. <a href="<?= PROOT; ?>prayer-time" class="link">Prayer time</a></p>
+                    <form class="col-md-7 bg-light border rounded-2 position-relative mx-auto p-2 mt-4 mt-md-5">
+                        <div class="input-group">
+                            <input class="form-control focus-shadow-none bg-light border-0 me-1" type="email" placeholder="Search blog">
+                            <button type="button" class="btn btn-dark rounded-2 mb-0"><i class="bi bi-search me-2"></i>Search</button>
+                        </div>
+                    </form>
+                </div>
+
+                <h6 class="mb-4">Blog category</h6>
+                <!-- Slider START -->
+                <div class="swiper" data-swiper-options='{
+                    "slidesPerView": 2, 
+                    "spaceBetween": 30,
+                    "pagination":{
+                        "el":".swiper-pagination",
+                        "clickable":"true"
+                    },
+                    "breakpoints": {
+                        "576": {"slidesPerView": 3},
+                        "992": {"slidesPerView": 5}
+                    }}'>
+
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide">
+                            <a href="#" class="card card-img-scale overflow-hidden">
+                                <img src="assets/images/blog/4by4/01.jpg" class="img-scale" alt="category-img">
+                                <div class="card-img-overlay">
+                                    <div class="badge text-bg-dark">Lifestyle</div>
+                                </div>
+                            </a>
+                        </div>
+
+                        <div class="swiper-slide">
+                            <a href="#" class="card card-img-scale overflow-hidden">
+                                <img src="assets/images/blog/4by4/02.jpg" class="img-scale" alt="category-img">
+                                <div class="card-img-overlay">
+                                    <div class="badge text-bg-dark">Technology</div>
+                                </div>
+                            </a>
+                        </div>
+
+                        <div class="swiper-slide">
+                            <a href="#" class="card card-img-scale overflow-hidden">
+                                <img src="assets/images/blog/4by4/03.jpg" class="img-scale" alt="category-img">
+                                <div class="card-img-overlay">
+                                    <div class="badge text-bg-dark">Design</div>
+                                </div>
+                            </a>
+                        </div>
+
+                        <div class="swiper-slide">
+                            <a href="#" class="card card-img-scale overflow-hidden">
+                                <img src="assets/images/blog/4by4/04.jpg" class="img-scale" alt="category-img">
+                                <div class="card-img-overlay">
+                                    <div class="badge text-bg-dark">Marketing</div>
+                                </div>
+                            </a>
+                        </div>
+
+                        <div class="swiper-slide">
+                            <a href="#" class="card card-img-scale overflow-hidden">
+                                <img src="assets/images/blog/4by4/05.jpg" class="img-scale" alt="category-img">
+                                <div class="card-img-overlay">
+                                    <div class="badge text-bg-dark">Research</div>
+                                </div>
+                            </a>
+                        </div>
                     </div>
+                    <div class="swiper-pagination swiper-pagination-primary position-relative mt-4"></div>
                 </div>
             </div>
         </section>
@@ -206,11 +281,10 @@
 
     <div class="back-top"></div>
 
-    <!-- <script type="text/javascript" src="<?= PROOT; ?>dist/js/jquery-3.7.1.min.js"></script>
+    <script type="text/javascript" src="<?= PROOT; ?>dist/js/jquery-3.7.1.min.js"></script>
     <script type="text/javascript" src="<?= PROOT; ?>dist/js/popper.min.js"></script>
-    <script type="text/javascript" src="<?= PROOT; ?>dist/js/bootstrap.min.js"></script> -->
+    <script type="text/javascript" src="<?= PROOT; ?>dist/js/bootstrap.min.js"></script>
 
-    <script src="<?= PROOT; ?>dist/js/bootstrap.bundle.min.js"></script>
 
 
     <script src="<?= PROOT; ?>dist/js/purecounter_vanilla.js"></script>
@@ -222,13 +296,6 @@
     <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js'></script>
 
     <script type="text/javascript">
-        document.addEventListener('DOMContentLoaded', function() {
-        var calendarEl = document.getElementById('calendar');
-        var calendar = new FullCalendar.Calendar(calendarEl, {
-          initialView: 'dayGridMonth'
-        });
-        calendar.render();
-      });
     </script>
 
 </body>
