f4fe9fbbca5718eed6b9d0e8ca99ff116ae4efc8
morotijani <tjhackx111@gmail.com>
2024-07-15 13:40:36 +0000

    initial commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,50 @@
+        <footer class="app-footer">
+                      <ul class="list-inline">
+            <li class="list-inline-item">
+              <a class="text-muted" href="#">Support</a>
+            </li>
+            <li class="list-inline-item">
+              <a class="text-muted" href="#">Help Center</a>
+            </li>
+            <li class="list-inline-item">
+              <a class="text-muted" href="#">Privacy</a>
+            </li>
+            <li class="list-inline-item">
+              <a class="text-muted" href="#">Terms of Service</a>
+            </li>
+          </ul>
+          <div class="copyright"> Copyright © 2018. All right reserved. </div>
+        </footer><!-- /.app-footer -->
+        <!-- /.wrapper -->
+      </main><!-- /.app-main -->
+    </div><!-- /.app -->
+    <!-- BEGIN BASE JS -->
+    <script src="<?= PROOT; ?>admin/dist/js/jquery.min.js"></script>
+    <script src="<?= PROOT; ?>admin/dist/js/popper.min.js"></script>
+    <script src="<?= PROOT; ?>admin/dist/js/bootstrap.min.js"></script> <!-- END BASE JS -->
+    <!-- BEGIN PLUGINS JS -->
+    <script src="<?= PROOT; ?>admin/dist/js/pace.min.js"></script>
+    <script src="<?= PROOT; ?>admin/dist/js/stacked-menu.min.js"></script>
+    <script src="<?= PROOT; ?>admin/dist/js/perfect-scrollbar.min.js"></script>
+    <script src="<?= PROOT; ?>admin/dist/js/flatpickr.min.js"></script>
+    <script src="<?= PROOT; ?>admin/dist/js/jquery.easypiechart.min.js"></script>
+    <script src="<?= PROOT; ?>admin/dist/js/Chart.min.js"></script> <!-- END PLUGINS JS -->
+    <!-- BEGIN THEME JS -->
+    <script src="<?= PROOT; ?>admin/dist/js/theme.min.js"></script> <!-- END THEME JS -->
+    <!-- BEGIN PAGE LEVEL JS -->
+    <script src="<?= PROOT; ?>admin/dist/js/dashboard-demo.js"></script> 
+    <!-- END PAGE LEVEL JS -->
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-116692175-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+
+      function gtag()
+      {
+        dataLayer.push(arguments);
+      }
+      gtag('js', new Date());
+      gtag('config', 'UA-116692175-1');
+    </script>
+  </body>
+</html>
--- admin/index.php
+++ admin/index.php
@@ -31,7 +31,7 @@
                                     <div class="metric-row metric-flush">
                                         <div class="col">
                                             <a href="user-teams.html" class="metric metric-bordered align-items-center">
-                                                <h2 class="metric-label"> Teams </h2>
+                                                <h2 class="metric-label"> Members </h2>
                                                 <p class="metric-value h3">
                                                     <sub><i class="oi oi-people"></i></sub> <span class="value">8</span>
                                                 </p>
@@ -39,88 +39,37 @@
                                         </div>
                                         <div class="col">
                                             <a href="user-projects.html" class="metric metric-bordered align-items-center">
-                                                <h2 class="metric-label"> Projects </h2>
+                                                <h2 class="metric-label"> Executives </h2>
                                                 <p class="metric-value h3">
                                                   <sub><i class="oi oi-fork"></i></sub> <span class="value">12</span>
                                                 </p>
                                             </a>
                                         </div>
                                         <div class="col">
-                                              <a href="user-tasks.html" class="metric metric-bordered align-items-center">
-                                    <h2 class="metric-label"> Active Tasks </h2>
-                                    <p class="metric-value h3">
-                                      <sub><i class="fa fa-tasks"></i></sub> <span class="value">64</span>
-                                    </p>
-                                  </a>
+                                            <a href="user-tasks.html" class="metric metric-bordered align-items-center">
+                                                <h2 class="metric-label"> Active Tasks </h2>
+                                                <p class="metric-value h3">
+                                                    <sub><i class="fa fa-tasks"></i></sub> <span class="value">64</span>
+                                                </p>
+                                            </a>
+                                        </div>
+                                    </div>
                                 </div>
-                              </div>
+                                <div class="col-lg-3">
+                                    <a href="user-tasks.html" class="metric metric-bordered">
+                                        <div class="metric-badge">
+                                            <span class="badge badge-lg badge-success"><span class="oi oi-media-record pulse mr-1"></span> ONGOING TASKS</span>
+                                        </div>
+                                        <p class="metric-value h3">
+                                            <sub><i class="oi oi-timer"></i></sub> <span class="value">8</span>
+                                        </p>
+                                    </a>
+                                </div>
                             </div>
-                            <div class="col-lg-3">
-                              <a href="user-tasks.html" class="metric metric-bordered">
-                                <div class="metric-badge">
-                                  <span class="badge badge-lg badge-success"><span class="oi oi-media-record pulse mr-1"></span> ONGOING TASKS</span>
-                                </div>
-                                <p class="metric-value h3">
-                                  <sub><i class="oi oi-timer"></i></sub> <span class="value">8</span>
-                        </p>
-                      </a> <!-- /.metric -->
-                    </div><!-- /metric column -->
-                  </div><!-- /metric row -->
-                </div><!-- /.section-block -->
-                <!-- grid row -->
+                        </div>
 
-
-              </div><!-- /.page-section -->
-            </div><!-- /.page-inner -->
-          </div><!-- /.page -->
-        </div><!-- .app-footer -->
-        <footer class="app-footer">
-          <ul class="list-inline">
-            <li class="list-inline-item">
-              <a class="text-muted" href="#">Support</a>
-            </li>
-            <li class="list-inline-item">
-              <a class="text-muted" href="#">Help Center</a>
-            </li>
-            <li class="list-inline-item">
-              <a class="text-muted" href="#">Privacy</a>
-            </li>
-            <li class="list-inline-item">
-              <a class="text-muted" href="#">Terms of Service</a>
-            </li>
-          </ul>
-          <div class="copyright"> Copyright © 2018. All right reserved. </div>
-        </footer><!-- /.app-footer -->
-        <!-- /.wrapper -->
-      </main><!-- /.app-main -->
-    </div><!-- /.app -->
-    <!-- BEGIN BASE JS -->
-    <script src="<?= PROOT; ?>admin/dist/js/jquery.min.js"></script>
-    <script src="<?= PROOT; ?>admin/dist/js/popper.min.js"></script>
-    <script src="<?= PROOT; ?>admin/dist/js/bootstrap.min.js"></script> <!-- END BASE JS -->
-    <!-- BEGIN PLUGINS JS -->
-    <script src="<?= PROOT; ?>admin/dist/js/pace.min.js"></script>
-    <script src="<?= PROOT; ?>admin/dist/js/stacked-menu.min.js"></script>
-    <script src="<?= PROOT; ?>admin/dist/js/perfect-scrollbar.min.js"></script>
-    <script src="<?= PROOT; ?>admin/dist/js/flatpickr.min.js"></script>
-    <script src="<?= PROOT; ?>admin/dist/js/jquery.easypiechart.min.js"></script>
-    <script src="<?= PROOT; ?>admin/dist/js/Chart.min.js"></script> <!-- END PLUGINS JS -->
-    <!-- BEGIN THEME JS -->
-    <script src="<?= PROOT; ?>admin/dist/js/theme.min.js"></script> <!-- END THEME JS -->
-    <!-- BEGIN PAGE LEVEL JS -->
-    <script src="<?= PROOT; ?>admin/dist/js/dashboard-demo.js"></script> 
-    <!-- END PAGE LEVEL JS -->
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-116692175-1"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-
-      function gtag()
-      {
-        dataLayer.push(arguments);
-      }
-      gtag('js', new Date());
-      gtag('config', 'UA-116692175-1');
-    </script>
-  </body>
-</html>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <?php include ("includes/footer.php"); ?>
