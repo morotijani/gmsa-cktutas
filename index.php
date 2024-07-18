81a50d74699de2efffa6cfebf74cf969b0d90aa2
morotijani <tjhackx111@gmail.com>
2024-07-18 13:35:32 +0000

    initial commit

--- index.php
+++ index.php
@@ -214,7 +214,7 @@
             <div class="container">
                 <div class="inner-container">
                     <!-- Title -->
-                    <h2 class="text-center mb-4 mb-sm-5">Our leadership</h2>
+                    <h2 class="text-center mb-4 mb-sm-5">Know your executives</h2>
 
                     <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-0 justify-content-center">
                         <!-- Team item -->
@@ -280,68 +280,7 @@
                             </div>
                         </div>
 
-                        <!-- Team item -->
-                        <div class="col">
-                            <div class="card card-body bg-transparent text-center p-4">
-                                <!-- Image -->
-                                <div class="avatar avatar-xxl mx-auto flex-shrink-0 mb-3">
-                                    <img class="avatar-img rounded-circle" src="assets/images/avatar/09.jpg" alt="avatar">
-                                </div>
-
-                                <!-- Content -->
-                                <h6 class="mb-1"><a href="#">Dennis Barrett</a></h6>
-                                <small>CEO &amp; Founder</small>
-
-                                <!-- Social button -->
-                                <ul class="list-inline mb-0 mt-3">
-                                    <li class="list-inline-item"> <a class="btn btn-xs btn-icon btn-light" href="#"><i class="fab fa-fw fa-facebook-f lh-base"></i></a> </li>
-                                    <li class="list-inline-item"> <a class="btn btn-xs btn-icon btn-light" href="#"><i class="fab fa-fw fa-instagram lh-base"></i></a> </li>
-                                    <li class="list-inline-item"> <a class="btn btn-xs btn-icon btn-light" href="#"><i class="fab fa-fw fa-twitter lh-base"></i></a> </li>
-                                </ul>
-                            </div>
-                        </div>
-
-                        <!-- Team item -->
-                        <div class="col">
-                            <div class="card card-body bg-transparent text-center p-4">
-                                <!-- Image -->
-                                <div class="avatar avatar-xxl mx-auto flex-shrink-0 mb-3">
-                                    <img class="avatar-img rounded-circle" src="assets/images/avatar/05.jpg" alt="avatar">
-                                </div>
-
-                                <!-- Content -->
-                                <h6 class="mb-1"><a href="#">Dennis Barrett</a></h6>
-                                <small>CEO &amp; Founder</small>
-
-                                <!-- Social button -->
-                                <ul class="list-inline mb-0 mt-3">
-                                    <li class="list-inline-item"> <a class="btn btn-xs btn-icon btn-light" href="#"><i class="fab fa-fw fa-facebook-f lh-base"></i></a> </li>
-                                    <li class="list-inline-item"> <a class="btn btn-xs btn-icon btn-light" href="#"><i class="fab fa-fw fa-instagram lh-base"></i></a> </li>
-                                    <li class="list-inline-item"> <a class="btn btn-xs btn-icon btn-light" href="#"><i class="fab fa-fw fa-twitter lh-base"></i></a> </li>
-                                </ul>
-                            </div>
-                        </div>
-
-                        <!-- Team item -->
-                        <div class="col">
-                            <div class="card card-body bg-transparent text-center p-4">
-                                <!-- Image -->
-                                <div class="avatar avatar-xxl mx-auto flex-shrink-0 mb-3">
-                                    <img class="avatar-img rounded-circle" src="assets/images/avatar/10.jpg" alt="avatar">
-                                </div>
-
-                                <!-- Content -->
-                                <h6 class="mb-1"><a href="#">Bryan Knight</a></h6>
-                                <small>Product Designer</small>
-
-                                <!-- Social button -->
-                                <ul class="list-inline mb-0 mt-3">
-                                    <li class="list-inline-item"> <a class="btn btn-xs btn-icon btn-light" href="#"><i class="fab fa-fw fa-facebook-f lh-base"></i></a> </li>
-                                    <li class="list-inline-item"> <a class="btn btn-xs btn-icon btn-light" href="#"><i class="fab fa-fw fa-instagram lh-base"></i></a> </li>
-                                    <li class="list-inline-item"> <a class="btn btn-xs btn-icon btn-light" href="#"><i class="fab fa-fw fa-twitter lh-base"></i></a> </li>
-                                </ul>
-                            </div>
-                        </div>
+                        
                     </div>
                 </div>
             </div>
