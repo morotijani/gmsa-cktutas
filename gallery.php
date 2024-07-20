2c7f08e8e0cb69dc0a0f0a59390774d5a2165fed
morotijani <tjhackx111@gmail.com>
2024-07-20 23:00:12 +0000

    initial commit

--- gallery.php
+++ gallery.php
@@ -47,14 +47,14 @@
             </div>
 
             <div class="container-fluid overflow-hidden">
-                <div class="row g-4 g-lg-3 justify-content-lg-between">
-                    <div class="col-6 col-sm-4 position-relative ms-lg-n5">               
+                <div class="row g-4 filter-container mt-3" data-isotope='{"layoutMode": "masonry"}'>
+                    <!-- <div class="col-6 col-sm-4 position-relative ms-lg-n5">               
                         <figure class="position-absolute top-0 end-0 me-n8 mt-4 z-index-1">
                             <svg width="222.93px" height="172.15px">
                                 <path class="fill-primary" d="M0,152.3C25.31,99.13,142.25,26.36,187.95,4.96c21.41-10.03,48.53-5.45,27.23,19.21 c-11.03,12.77-28.92,28.13-37.16,35.18c-20.39,17.45-46.34,42.82-64.05,67.05c-13.98,19.11-10.02,14.46,6.64,0.11 c13.57-11.69,29.17-25.12,40.65-32.05c3.52-2.13,6.77-3.66,9.64-4.43c9.07-2.42,13,3.08,9.55,10.77 c-7.7,17.16-22.46,31.32-31.36,47.7c-4.97,9.14-13.24,28.13,7.7,16.79c3.8-2.05,7.36-4.37,9.99-5.45l0.79,1.35 c-2.43,0.88-36.48,26.3-27.52-3.45c5.4-17.94,19.83-32.81,29.27-48.73c5.34-9,8.21-13.61-3.32-6.64 c-10.7,6.46-26.03,19.67-39.37,31.15c-15.56,13.4-28.68,24.69-33.81,21.53c-11.43-7.04,27.26-51,35.01-59.53 c14.77-16.28,30.38-31.21,45.8-44.61c6.33-5.48,18.43-16.43,28.35-25.25c11.62-10.34,15.39-16.14-5.89-6.18 C152.63,39.83,42.78,111.18,18.54,162.1L0,152.3z"/>
                             </svg>
                         </figure>
-                    </div>
+                    </div> -->
 
                     <?php if (isset($_GET['date']) && !empty($_GET['date'])):
                         $search = sanitize($_GET['date']);
@@ -82,7 +82,7 @@
                         } else {
                             echo '
                                 <div class="alert alert-info">
-                                    No data found!
+                                    No data found in gallery!
                                 </div>
                             ';
                         }
@@ -90,15 +90,15 @@
                     <?php else: ?>
                         <?php if ($count_rows > 0): ?>
                             <?php foreach ($rows as $row): ?>
-                                <div class="col-sm-4 position-relative order-2 order-sm-1 z-index-9">
-                                    <a data-glightbox data-gallery="gallery" href="<?= PROOT . $row["gallery_media"]; ?>">
-                                        <div class="card card-element-hover card-overlay-hover overflow-hidden">
-                                            <img src="<?= PROOT . $row["gallery_media"]; ?>" class="rounded-3" alt="">
-                                            <div class="hover-element w-100 h-100">
+                                <div class="col-sm-4 col-lg-3 grid-item marketing business brand">
+                                    <div class="card card-element-hover card-metro-hover">
+                                         <a data-glightbox data-gallery="gallery" href="<?= PROOT . $row["gallery_media"]; ?>">
+                                            <img src="<?= PROOT . $row["gallery_media"]; ?>" alt="portfolio-img">  
+                                            <div class="card-img-overlay hover-element d-flex">
                                                 <i class="bi bi-fullscreen fs-6 text-white position-absolute top-50 start-50 translate-middle bg-dark rounded-1 p-2 lh-1"></i>
                                             </div>
-                                        </div>
-                                    </a>
+                                        </a>                    
+                                    </div>
                                 </div>
                             <?php endforeach; ?>
                         <?php else: ?>
@@ -115,3 +115,4 @@
 <?php include ("inc/footer.inc.php"); ?>
     <script type="text/javascript" src="<?= PROOT; ?>dist/js/glightbox.js"></script>
     <script src="<?= PROOT; ?>dist/js/functions.js"></script>
+    <script type="text/javascript" src="<?= PROOT; ?>dist/js/isotope.pkgd.min.js"></script>
