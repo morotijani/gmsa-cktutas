3da2225dbf3f43094191a68c64ee0d1160db63d6
morotijani <tjhackx111@gmail.com>
2024-07-21 08:49:49 +0000

    initial commit

---
+++ tc.php
@@ -0,0 +1,34 @@
+<?php
+
+    // Terms and conditions page
+
+    require_once ("db_connection/conn.php");
+    $TITLE = "Privacy Policy";
+    $navTheme = "";
+    include ("inc/header.inc.php");
+    include ("inc/nav.inc.php");
+
+
+?>
+    <main>
+        <section class="pt-8 pt-xl-9">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-10 text-center mx-auto">
+                        <span class="font-heading heading-color d-inline-block bg-light px-3 py-2 rounded-3 mb-4">🕌 GMSA</span>
+                        <h1 class="mb-4">Terms and Conditions</h1>
+                        <p class="lead mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+       
+    </main>
+<?php include ("inc/footer.inc.php"); ?>
