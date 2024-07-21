44fed1310af76ffc627187332130f55d0ff49cc6
morotijani <tjhackx111@gmail.com>
2024-07-21 08:48:48 +0000

    initial commit

--- privacy-policy.php
+++ privacy-policy.php
@@ -1,9 +1,9 @@
 <?php
 
-    // Contact us page
+    // Privacy policy page
 
     require_once ("db_connection/conn.php");
-    $TITLE = "About us";
+    $TITLE = "Privacy Policy";
     $navTheme = "";
     include ("inc/header.inc.php");
     include ("inc/nav.inc.php");
@@ -16,8 +16,13 @@
                 <div class="row">
                     <div class="col-xl-10 text-center mx-auto">
                         <span class="font-heading heading-color d-inline-block bg-light px-3 py-2 rounded-3 mb-4">🕌 GMSA</span>
-                        <h1 class="mb-4">We are Ghana Muslim Student Association, CKT-UTAS</h1>
-                        <p class="lead mb-0"><?= nl2br($site_row['about_info']); ?></p>
+                        <h1 class="mb-4">Our Privacy Policy</h1>
+                        <p class="lead mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
                     </div>
                 </div>
