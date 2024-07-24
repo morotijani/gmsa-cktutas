1c2dd3ecef343947f01b179d365af44573b3dc81
morotijani <tjhackx111@gmail.com>
2024-07-24 19:48:56 +0000

    initial commit

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -624,7 +624,7 @@ function admin_is_logged_in(){
 // Redirect admin if !logged in
 function admn_login_redirect($url = 'login') {
 	$_SESSION['flash_error'] = 'You must be logged in to access that page!';
-	redirect(PROOT . 'admin/' . $url);
+	redirect(PROOT . 'admin/auth/' . $url);
 }
 
 // Redirect admin if do not have permission
