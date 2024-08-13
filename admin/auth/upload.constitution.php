942974bcd1b3216d008d2d707514e9ed6804cfd3
morotijani <tjhackx111@gmail.com>
2024-08-13 14:30:50 +0000

    initial commit

--- admin/auth/upload.constitution.php
+++ admin/auth/upload.constitution.php
@@ -4,25 +4,25 @@
 
 	require_once ("../../db_connection/conn.php");
 
-	if ($_FILES["signature"]["name"]  != '') {
+	if ($_FILES["constitution"]["name"]  != '') {
 
-		$test = explode(".", $_FILES["signature"]["name"]);
+		$test = explode(".", $_FILES["constitution"]["name"]);
 
 		$extention = end($test);
 
 		$name = rand(100, 999) . '.' . $extention;
 
-		$nameLoc = 'assets/media/signature/' . $name;
+		$nameLoc = 'assets/media/constitution/' . $name;
 
 		$location = BASEURL . $nameLoc;
 
-		$move = move_uploaded_file($_FILES["signature"]["tmp_name"], $location);
+		$move = move_uploaded_file($_FILES["constitution"]["tmp_name"], $location);
 
 		if ($move) {
 			// code...
 			$query = "
 				UPDATE gmsa_about 
-				SET signature = ?
+				SET constitution = ?
 			";
 			$statement = $conn->prepare($query);
 			$result = $statement->execute([$nameLoc]);
@@ -30,7 +30,7 @@
 			if ($result) {
 				// code...
 
-				$log_message = "update admin signature";
+				$log_message = "update admin constitution";
                 add_to_log($log_message, $admin_data['admin_id']);
 
 				echo '
@@ -38,7 +38,7 @@
 						<div class="list-group-item">
 							<div class="list-group-item-figure">
 								<div class="tile tile-img">
-									<img src="' . PROOT . 'assets/media/signature/'.$name.'" width="32" height="32" />
+									<img src="' . PROOT . 'assets/media/constitution/'.$name.'" width="32" height="32" />
 									<input type="hidden" name="uploaded_image" id="uploaded_image" value="' . $location . '">
 								</div>
 							</div>
@@ -46,7 +46,7 @@
 							<div class="media align-items-center">
 								<div class="media-body">' . $name .'</div>
 									<div class="media-actions">
-										<button type="button" class="btn btn-sm btn-secondary removeSignature" id="' . $location . '">Remove</button>
+										<button type="button" class="btn btn-sm btn-secondary removeConstitution" id="' . $location . '">Remove</button>
 									</div>
 								</div>
 							</div>
