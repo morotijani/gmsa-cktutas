ca43566bd8938d17abe9ef8871815461252008a0
morotijani <tjhackx111@gmail.com>
2024-08-14 14:45:54 +0000

    intial commit

--- admin/site.php
+++ admin/site.php
@@ -144,18 +144,14 @@
                                     <div id="upload-constitution"></div>
                                     <?php if ($site_row['constitution'] != ''): ?>
                                         <div id="removeTempuploadedConstitution" class="list-group list-group-flush list-group-divider">
-                                            <div class="list-group-item">
-                                                <div class="list-group-item-figure">
-                                                    <div class="tile tile-img">
-                                                        <img src="<?= PROOT . $site_row['constitution']; ?>" width="32" height="32" />
-                                                    </div>
+                                            <div class="card">
+                                                <div class="card-body">
+                                                    <iframe src="https://docs.google.com/gview?url=<?= 'https://sites.local/' . $site_row['constitution']; ?>&embedded=true" style="width:200px; height:200px;" frameborder="0"></iframe>
                                                 </div>
-                                                <div class="list-group-item-body">
-                                                <div class="media align-items-center">
-                                                    <div class="media-body"><?= $site_row['constitution']; ?></div>
-                                                        <div class="media-actions">
-                                                            <button type="button" class="btn btn-sm btn-secondary removeConstitution" id="<?= BASEURL . $site_row['constitution']; ?>">Remove</button>
-                                                        </div>
+                                                <div class="d-flex justify-content-between p-3">
+                                                    <a href="<?= PROOT . $site_row['constitution']; ?>">Preview . <?= $site_row['constitution']; ?></a>
+                                                    <div class="media-actions">
+                                                        <button type="button" class="btn btn-sm btn-secondary removeConstitution" id="<?= BASEURL . $site_row['constitution']; ?>">Remove</button>
                                                     </div>
                                                 </div>
                                             </div>
