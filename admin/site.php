03805bb047ba9ef3328237b74b6bbfdebabd8007
morotijani <tjhackx111@gmail.com>
2024-08-14 14:50:59 +0000

    initial commit

--- admin/site.php
+++ admin/site.php
@@ -155,9 +155,7 @@
                                                 </div>
                                                 <div class="d-flex justify-content-between p-3">
                                                     <a href="<?= PROOT . $site_row['constitution']; ?>">Preview . <?= $site_row['constitution']; ?></a>
-                                                    <div class="media-actions">
-                                                        <button type="button" class="btn btn-sm btn-secondary removeConstitution" id="<?= BASEURL . $site_row['constitution']; ?>">Remove</button>
-                                                    </div>
+                                                    <button type="button" class="btn btn-sm btn-secondary removeConstitution" id="<?= BASEURL . $site_row['constitution']; ?>">Remove</button>
                                                 </div>
                                             </div>
                                         </div>
